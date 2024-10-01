35604605669e115386c93ca7601dc38d5accf0af
Amir Hashmi <144407626+AmirHashmi017@users.noreply.github.com>
2024-10-01 09:46:44 +0500

    Add files via upload

--- Lab 04 solution/K-Select Problem.py
+++ Lab 04 solution/K-Select Problem.py
@@ -1,7 +1,25 @@
 def KSelect(Arr,k):
-    pivot=Partition(Arr,0,len(Arr))
-    l=pivot-1
-    r=len(Arr)-(pivot+1)
-    if(r==k-1):
-        return Arr[pivot]
-    if(pivot>k-1):
+    pivot_index=Partition(Arr,0,len(Arr))
+    pivotvalue=Arr[pivot_index]
+    leftarr=Arr[:pivot_index]
+    rightarr=Arr[pivot_index+1:]
+    if(len(leftarr)==k-1):
+        return pivotvalue
+    elif(len(leftarr)>k-1):
+        return KSelect(Arr,k)
+    elif(len(leftarr)<k-1):
+        return KSelect(rightarr,k-len(leftarr)-1)
+    
+def Partition(Arr,start,end):
+    end_pivot=Arr[end-1]
+    i=start-1
+    for j in range(0,end-1,1):
+        if(Arr[j]<=end_pivot):
+            i+=1
+            Arr[i],Arr[j]=Arr[j],Arr[i]
+    Arr[i+1],Arr[end-1]=Arr[end-1],Arr[i+1]
+    return i+1
+
+arr = [12, 3, 5, 7, 19, 10]
+k = 4
+print(f"The {k}-th smallest element is: {KSelect(arr, k)}")
